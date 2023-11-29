1a0724f6cf5b6b287d2de671ce793f29cb011b7e
Voyager_lqg7 <stepanenko.vadik@bk.ru>
2023-11-29 13:56:12 +0100

    Paddle realization

--- Arkanoid/paddle.h
+++ Arkanoid/paddle.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include<QImage>
+#include<QRect>
+
+class Paddle {
+public:
+	Paddle();
+	~Paddle();
+
+	void resetState();
+	void move();
+	void setDx(int);
+	QRect getRect();
+	QImage& getImage();
+private:
+	QImage image;
+	QRect rect;
+	int dx;
+	static const int INITIAL_X = 200;
+	static const int INITIAL_Y = 360;
+};
